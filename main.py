ec00d91c7c232a6af48a92297291a899fcb0d98b
Luke Napierkowski <lukejnapierkowski@gmail.com>
2021-03-04 02:24:15 +0000

    Data loads into csv correctly but length is limited not sure if that is inherent or just due to repl.it limitations

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import numpy as np
 
 if __name__ == "__main__":
   totalGames = 0
+  cont = ""
   while 1:
     plays = []
     numPlays = 0
     game = gamedata.gameData(4)
     while 1:
-      game.printSituation()
-      input("Press enter...")
+      #game.printSituation()
+      #input("Press enter...")
       move = game.chooseMove()
       print(move.toString())
       if len(plays) > 0:
@@ -17,22 +18,30 @@ if __name__ == "__main__":
       else:
         plays = game.getPlay(move.index)
       numPlays += 1
-      input("Press enter...")
+      #input("Press enter...")
       game.executeMove(move)
       outcome = game.postMove()
       if outcome >= 0:
         break
 
-    if outcome > 100000:
-      input("Win "+str(outcome))
-    elif outcome >= 0:
-      input("Loss "+str(outcome))
-    outcomes = np.full((numPlays,1),outcome)
-    plays = np.hstack((plays, outcomes))
-    np.savetxt("dataset.csv", plays, delimiter=",")
+    if outcome > 100000 and cont!="just run":
+      cont=input("Win "+str(outcome))
+    elif outcome >= 0 and cont!="just run":
+      cont=input("Loss "+str(outcome))
+    if numPlays > 1:
+      outcomes = np.full((numPlays,1),outcome)
+      plays = np.hstack((plays, outcomes))
+    else:
+      plays = np.append(plays, [outcome])
+    #for play in plays:
+    #  print(play)
+    #  input("press enter...")
+    with open("dataset.csv",'a') as dataFile:
+      np.savetxt(dataFile, plays)
+    #np.savetxt("dataset.csv", plays, delimiter=",")
 
     totalGames += 1
-    if totalGames > 1:
+    if totalGames > 1000000:
       break
 
 
